ddc202ea80daad965da52536b75616bd49bfd5f6
Anubhav <anubhav675@gmail.com>
2026-09-02 14:30:42 +0300

    refactor: render page links from data

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
+import PageLinks from "./PageLinks";
 const Footer = () => {
   return (
     <footer className="section footer">
-      <ul className="footer-links" id="nav-links">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+        {/* <ul>
         <li><a href="#home" className="footer-link">home</a></li>
         <li><a href="#about" className="footer-link">about</a></li>
         <li><a href="#services" className="footer-link">services</a></li>
         <li><a href="#tours" className="footer-link">tours</a></li>
-      </ul>
+      </ul> */}
       <ul className="footer-icons">
         <li>
           <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="footer-icon">
