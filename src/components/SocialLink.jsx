da60c740a079b263c67112835fd0becbd115b24e
Anubhav <anubhav675@gmail.com>
2026-09-02 15:01:14 +0300

    refactor navlink

--- src/components/SocialLink.jsx
+++ src/components/SocialLink.jsx
@@ -1,5 +1,17 @@
-function SocialLink() {
-  return <div>SocialLink</div>;
+// import {socialLink} from "../data";
+
+function SocialLink({ link, itemClass }) {
+  return (
+    <li>
+      <a        href={link.href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className={itemClass}>
+      
+      <i className={link.icon}></i>
+      </a>
+    </li>
+  );
 }
 
 export default SocialLink;
